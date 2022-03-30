4dbfba0963527b758c4b785cb64bf45132c2cffb
rebeccapeltz <rebecca.peltz@cloudinary.com>
2022-03-30 07:17:04 -0700

    use react sdk for avatar'

--- components/avatar.js
+++ components/avatar.js
@@ -1,8 +1,35 @@
-import { Image } from 'cloudinary-react';
+// Import React SDK and Url Generator
+import { AdvancedImage } from '@cloudinary/react';
+import { Cloudinary } from '@cloudinary/url-gen';
+
+// Import required actions and qualifiers.
+import {thumbnail} from "@cloudinary/url-gen/actions/resize";
+import {focusOn} from "@cloudinary/url-gen/qualifiers/gravity";
+import {FocusOn} from "@cloudinary/url-gen/qualifiers/focusOn";
+
 export default function Avatar({ name, picture, publicid, cloudname }) {
+  const cld = new Cloudinary({
+    cloud: {
+      cloudName: cloudname,
+      secure: true,
+    },
+  });
+
+  const cldImage = cld.image(publicid);
+  cldImage.resize(
+    thumbnail().width(150).height(150).gravity(focusOn(FocusOn.face()))
+  );
+  cldImage.quality('auto');
+  cldImage.format('auto');
+
   return (
     <div className='flex items-center'>
-      <Image
+      <AdvancedImage
+        className='w-12 h-12 rounded-full mr-4'
+        cldImg={cldImage}
+      />
+
+      {/* <Image
         className='w-12 h-12 rounded-full mr-4'
         alt={name}
         publicId={publicid}
@@ -13,7 +40,7 @@ export default function Avatar({ name, picture, publicid, cloudname }) {
         gravity='auto'
         fetch_format='auto'
         secure='true'
-      />
+      /> */}
       <div className='text-xl font-bold'>{name}</div>
     </div>
   );

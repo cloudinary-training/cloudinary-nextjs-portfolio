88f85c2c1a869f8f15bf13f55487c9bac4aa6a83
rebeccapeltz <rebecca.peltz@cloudinary.com>
2022-04-05 11:39:09 -0700

    update avatar to use CloudinaryImage

--- components/avatar.js
+++ components/avatar.js
@@ -1,6 +1,6 @@
 // Import React SDK and Url Generator
 import { AdvancedImage } from '@cloudinary/react';
-import { Cloudinary } from '@cloudinary/url-gen';
+import { CloudinaryImage } from '@cloudinary/url-gen';
 
 // Import required actions and qualifiers.
 import { thumbnail } from '@cloudinary/url-gen/actions/resize';
@@ -8,14 +8,7 @@ import { focusOn } from '@cloudinary/url-gen/qualifiers/gravity'; // action
 import { FocusOn } from '@cloudinary/url-gen/qualifiers/focusOn'; // qualifier
 
 export default function Avatar({ name, publicid, cloudname }) {
-  const cld = new Cloudinary({
-    cloud: {
-      cloudName: cloudname,
-      secure: true,
-    },
-  });
-
-  const cldImage = cld.image(publicid);
+  const cldImage = new CloudinaryImage(publicid, {cloudName: cloudname, analytics: false});
   cldImage
     .resize(thumbnail().width(150).height(150).gravity(focusOn(FocusOn.face())))
     .quality('auto')
